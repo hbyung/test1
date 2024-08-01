7a77b65a344c8e087331849baef4eb3fb9f3c534
byung hhwan <tjsdnd627@gmail.com>
2024-08-01 16:20:21 +0900

    feat: first commit 전체변경

--- src/test/java/RedisTS/demo/DemoApplicationTests.java
+++ src/test/java/RedisTS/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package RedisTS.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package RedisTS.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
